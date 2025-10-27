ed30fd5bf4581310a05d98a010ce52f6134b3b56
KKrntzk <sophie.kornetzki@gmail.com>
2025-10-27 09:06:54 +0100

    gallery array added to js with imgs

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const smallImg = [];
+const gallery = [
+  "./img/castle-7728772_1280.png",
+  "./imgedinburgh-view-4959200_1280.png",
+  "./img/eilean-donan-castle-4785333_1280.png",
+  "./img/landscape-5071102_1280.png",
+  "./img/landscape-5071102_1280.png",
+  "./img/neist-point-540119_1280.png",
+  "./img/road-4955680_1280.png",
+  "./img/scotland-2790177_1280.png",
+  "./img/scotland-4862039_1280.png",
+  "./img/sea-192989_1280.png",
+  "./img/skye-5066493_1280.png",
+  "./img/waterfall-4949231_1280.png",
+  "./img/waterfall-9828788_1280.png",
+];
+
+function renderImgOnload() {
+  const containerRef = document.getElementById("imgContainer");
+
+  for (let counter = 0; counter < gallery.length; counter++) {
+    containerRef.innerHTML += `<div class`;
+  }
+}
